c00465ecb665d1eadf9bf0bd3c6bea6f90df1043
Nebiyu <nebadan7@gmail.com>
2022-12-16 00:43:56 -0800

    done

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a req to $1 URL, display response status code only
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# bash script to display status code of server
+curl -L -s -X HEAD -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST req to URL $!, display response body
-curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"
+# bash script for posting json data files and testing a server
+curl -s -d "@$2" -H "Content-Type: application/json" -X POST "$1"
